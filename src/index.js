2909f58633115095ce8aee9932928cd72a9e5ac6
Md. Hasibul Hasan <abeer.rocky@gmail.com>
2023-03-07 00:22:12 +0600

    first

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render( <
     React.StrictMode >
     <
-    h1 > hello this my app < /h1>  <
+    h1 > hello this my app <
+    /h1>   <
     /React.StrictMode>
 );
 
